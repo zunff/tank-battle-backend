b15a8aa4be09d4a9003483cc325a7636fe3f4337
zunf <1430119384@qq.com>
2025-12-12 17:16:38 +0800

    first commit

---
+++ src/main/java/com/zunf/tankbattlebackend/TankBattleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zunf.tankbattlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankBattleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TankBattleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zunf/tankbattlebackend/TankBattleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zunf.tankbattlebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankBattleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

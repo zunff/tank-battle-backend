1ce74f40a9a7cf1636c9b8af8de17cb2009ea78b
zunf <1430119384@qq.com>
2025-12-25 17:59:34 +0800

    refactor(room): 房间管理逻辑从backend移动到tcpServer

---
+++ src/main/java/com/zunf/tankbattlebackend/service/grpc/UserGrpcService.java
@@ -0,0 +1,54 @@
+package com.zunf.tankbattlebackend.service.grpc;
+
+import cn.hutool.core.collection.CollUtil;
+import com.zunf.tankbattlebackend.common.ErrorCode;
+import com.zunf.tankbattlebackend.grpc.CommonProto;
+import com.zunf.tankbattlebackend.grpc.user.UserProto;
+import com.zunf.tankbattlebackend.grpc.user.UserServiceGrpc;
+import com.zunf.tankbattlebackend.model.entity.User;
+import com.zunf.tankbattlebackend.service.UserService;
+import com.zunf.tankbattlebackend.utils.ProtoBufUtil;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@GrpcService
+public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
+
+    @Resource
+    private UserService userService;
+
+    @Override
+    public void getUser(UserProto.GetUserRequest request, StreamObserver<CommonProto.BaseResponse> responseObserver) {
+        long playerId = request.getPlayerId();
+        if (playerId <= 0) {
+            responseObserver.onNext(ProtoBufUtil.baseCodeResp(ErrorCode.INVALID_ARGUMENT));
+            responseObserver.onCompleted();
+        }
+        User user = userService.getById(playerId);
+        if (user == null) {
+            responseObserver.onNext(ProtoBufUtil.baseCodeResp(ErrorCode.NOT_FOUND));
+            responseObserver.onCompleted();
+        }
+        UserProto.GetUserResponse resp = UserProto.GetUserResponse.newBuilder()
+                .setUser(UserProto.UserInfo.newBuilder().setPlayerId(user.getId()).setNickname(user.getNickname()).build())
+                .build();
+        responseObserver.onNext(ProtoBufUtil.successResp(resp.toByteString()));
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void listUser(UserProto.ListUserRequest request, StreamObserver<CommonProto.BaseResponse> responseObserver) {
+        List<Long> playerIdsList = request.getPlayerIdsList();
+        if (CollUtil.isEmpty(playerIdsList)) {
+            responseObserver.onNext(ProtoBufUtil.baseCodeResp(ErrorCode.INVALID_ARGUMENT));
+        }
+        List<User> userList = userService.listByIds(playerIdsList);
+        List<UserProto.UserInfo> userInfoList = userList.stream().map(user -> UserProto.UserInfo.newBuilder().setPlayerId(user.getId()).setNickname(user.getNickname()).build()).toList();
+        UserProto.ListUserResponse resp = UserProto.ListUserResponse.newBuilder().addAllUsers(userInfoList).build();
+        responseObserver.onNext(ProtoBufUtil.successResp(resp.toByteString()));
+        responseObserver.onCompleted();
+    }
+}
